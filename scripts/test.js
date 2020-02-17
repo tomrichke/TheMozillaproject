c6e91aab24f72e0b814dfd2fde9e27800e8f8af0
tomrichu <rintase@gmail.com>
2020-02-17 12:47:50 +0300

    -mainjs -testjs - links added to index

---
+++ scripts/test.js
@@ -0,0 +1,8 @@
+let myhtml = document.querySelector('p');
+myhtml.onclick = function() {
+    alert('click the link below first !');
+}
+
+document.querySelector('h1').onclick = function() {
+    alert('Save your progress first !');
+}
